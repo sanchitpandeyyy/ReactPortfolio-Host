bad224bbbaeb74b16d8200bd226f81fea44d0429
Your Name <Sanchit Pandey>
2023-12-28 19:05:57 +0545

    skills page changes

--- src/components/skill/skill.js
+++ src/components/skill/skill.js
@@ -5,71 +5,81 @@ import Fade from 'react-reveal/Fade';
 
 const skill = () => {
   return (
- 
-    <section id='skill' className='gap-1 mt-3 overflow-hidden w-screen flex flex-col text-center my-0 mx-auto'>
-         <Fade left>
-       <span className='mt-5'>My Skills</span><br/>
-       <span className='text-7xl font-black mb-5'>My Skills</span>
-       <span className='px-12 py-2' >I own a diverse range of talents and expertise that I have developed over the years. From technical skills such as programming, and Web Development, to creative abilities like Graphics Design, UI/UX and Content Writing.</span>
-     
-     <div id='row1' className='gap-24 overflow-hidden w-screen flex justify-center p-20  '>
-       <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input className='' type="range"  min={0}  max={100}  value={80}  disabled />
-       <span>80%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>HTML5</p>
-      </div>
 
-      <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input type="range"  min={0}  max={100}  value={70}  disabled />
-       <span> 70%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>CSS3</p>
-      </div>
-
-      <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input type="range"  min={0}  max={100}  value={50}  disabled />
-       <span> 50%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>Photoshop</p>
-      </div>
+    <section id='skill' className='gap-1 mt-3 overflow-hidden w-screen flex flex-col text-center my-0 mx-auto justify-center items-center'>
+      <Fade left>
+        <span className='mt-5'>My Skills & Tools</span><br />
+        <span className='text-7xl font-black mb-5'>My Skills</span>
+        <span className='px-12 py-2' > I own a diverse range of talents and expertise that I have developed over the years. From technical skills such as programming, and Web Development, to creative abilities like Graphics Design, UI/UX and Content Writing. </span>
+        
 
 
-      </div>
-
-      <div id='row2' className='-mt-12 gap-24 overflow-hidden w-screen flex justify-center p-20 '>
-      
-      <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input type="range"  min={0}  max={100}  value={60}  disabled />
-       <span> 60%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>JavaScript</p>
-      </div>
-      
-      
-       <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input type="range"  min={0}  max={100}  value={65}  disabled />
-       <span> 65%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>ReactJS</p>
-      </div>
-
-      <div className='bg-blue-950 w-fit h-20 rounded-full flex flex-col justify-center items-center p-5'>
-       <div className='gap-1 flex justify-center font-bold text-yellow-400'>
-       <input type="range"  min={0}  max={100}  value={85}  disabled />
-       <span> 85%</span>
-       </div>
-       <p className='font-bold text-yellow-400'>TailWind CSS</p>
-      </div>
+           <div className='tools'>
+            
+              <img className=' '        
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+                     alt="bootstrap"
+             />
 
 
-      </div>
-  
+              <img className='  '
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                   alt="css3"
+              />
+            
+
+            
+
+              <img className='  '
+                src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+
+                  alt="firebase"
+              />
+            
+            
+
+              <img className='  '
+                src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+                 alt="git"
+              />
+            
+            
+
+              <img className='  '
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                alt="html5"
+              />
+            
+
+              <img className='  '
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                alt="js"
+              />
+            
+            
+
+              <img className='  '
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg"
+                alt="photoshop"
+              />
+            
+            
+
+              <img className='  '
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                alt="react"
+              />
+            
+            
+
+              <img className='  '
+                src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                alt="tailwind"
+              />
+
+         </div>
+
+
       </Fade>
 
     </section>
